9ae472c90e47953e6c7eee85ffc97de7198f9474
Afonso Matos <contact@afonsomatos.com>
2016-07-29 08:33:13 +0100

    Fix #1
    Quit button now destroys itself it the build is not standalone

---
+++ Assets/Scripts/StandaloneOnly.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class StandaloneOnly : MonoBehaviour {
+
+    void Start()
+    {
+        // #1
+        #if (!UNITY_STANDALONE)
+            Destroy(gameObject);
+        #endif
+    }
+}
